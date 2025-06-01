0868321f46df74abbd647354c87f56ef80ae3a32
bipin9178 <bipinpkanzariya@gmail.com>
2025-06-01 11:37:41 +0000

    variables declare

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=144553
+let accountEmail="bipin@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+accountEmail = "bipin12@gmail.com"
+accountPassword ="212121"
+accountCity = "bengaluru"
+//accountId = 2
+
+console.log(accountId);
+/*
+
+Perefer not to use var
+because  of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
+
